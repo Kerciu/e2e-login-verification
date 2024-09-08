d7845d945c43c01369165e2b2f22aec4f0be6d29
Kerciu <Kerciuuu@gmail.com>
2024-09-08 11:19:15 +0200

    configured the project

---
+++ src/main/java/com/end2end/springbootend2end/SpringBootEnd2endApplication.java
@@ -0,0 +1,13 @@
+package com.end2end.springbootend2end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEnd2endApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEnd2endApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/end2end/springbootend2end/SpringBootEnd2endApplicationTests.java
@@ -0,0 +1,13 @@
+package com.end2end.springbootend2end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEnd2endApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

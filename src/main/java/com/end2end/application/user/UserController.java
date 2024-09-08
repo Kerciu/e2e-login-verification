6c5e5a25e219cef27426a7bedb33ed23b712debe
Kerciu <Kerciuuu@gmail.com>
2024-09-08 12:03:29 +0200

    creates user controller class mapping the users

---
+++ src/main/java/com/end2end/application/user/UserController.java
@@ -0,0 +1,22 @@
+package com.end2end.application.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserServiceProvider userService;
+
+    @GetMapping
+    public String getUsers(Model model)
+    {
+        model.addAttribute("user", userService.getAllUsers());
+        return "users";
+    }
+}

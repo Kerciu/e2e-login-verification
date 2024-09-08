e34e05ccfd7d16f95eb6097b555ff97bf3f6de39
Kerciu <Kerciuuu@gmail.com>
2024-09-08 11:35:18 +0200

    created many to many relation ship between user and role, added join annotations

--- src/main/java/com/end2end/application/user/User.java
+++ src/main/java/com/end2end/application/user/User.java
@@ -1,17 +1,32 @@
 package com.end2end.application.user;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Collection;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String firstName;
     private String lastName;
     private String password;
     private String email;
-
     private boolean isActive = false;
 
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles",
+        joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
     private Collection<Role> roles;
 
 }

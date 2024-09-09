342bcfd315e872fe44bd3316a70331220554c0c2
Kerciu <Kerciuuu@gmail.com>
2024-09-09 10:28:09 +0200

    created registration form show method and user register method

--- src/main/java/com/end2end/application/registration/RegistrationController.java
+++ src/main/java/com/end2end/application/registration/RegistrationController.java
@@ -1,8 +1,13 @@
 package com.end2end.application.registration;
 
+import com.end2end.application.user.User;
 import com.end2end.application.user.UserServiceProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,4 +15,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class RegistrationController {
     private final UserServiceProvider userService;
+
+    @GetMapping("/registration-form")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new RegistrationRequest());
+        return "registration";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute("user") RegistrationRequest registrationRequest)
+    {
+        User user = userService.registerUser(registrationRequest);
+
+        // publish the verification email event
+
+        return "redirect:/registration//registration-form?success";
+    }
 }
--- src/main/java/com/end2end/application/user/UserServiceProvider.java
+++ src/main/java/com/end2end/application/user/UserServiceProvider.java
@@ -3,7 +3,6 @@ package com.end2end.application.user;
 import com.end2end.application.registration.RegistrationRequest;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserServiceProvider {
     List<User> getAllUsers();

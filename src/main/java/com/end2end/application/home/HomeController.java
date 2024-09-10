9797d19e1bc4194730b4486edcaa818b6c935756
Kerciu <Kerciuuu@gmail.com>
2024-09-10 11:17:23 +0200

    created html file and added login mapping in the home controller

--- src/main/java/com/end2end/application/home/HomeController.java
+++ src/main/java/com/end2end/application/home/HomeController.java
@@ -14,4 +14,9 @@ public class HomeController {
     public String showHomePage() {
         return "home";
     }
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
 }

7a54a1ba3fcfb89f8722c62821829c392907deb5
Kerciu <Kerciuuu@gmail.com>
2024-09-10 10:43:15 +0200

    started creating the home page for the application

---
+++ src/main/java/com/end2end/application/home/HomeController.java
@@ -0,0 +1,17 @@
+package com.end2end.application.home;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class HomeController {
+
+    @GetMapping
+    public String showHomePage() {
+        return "home";
+    }
+}

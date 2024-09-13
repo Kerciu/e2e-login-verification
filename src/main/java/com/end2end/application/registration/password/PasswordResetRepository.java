935469261e6de752d1407661ed9a335382f575b5
Kerciu <Kerciuuu@gmail.com>
2024-09-13 10:18:12 +0200

    created password reset token entity

--- src/main/java/com/end2end/application/registration/password/PasswordResetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.end2end.application.registration.password;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class PasswordResetRepository {
-}
--- src/main/java/com/end2end/application/registration/password/PasswordResetToken.java
+++ src/main/java/com/end2end/application/registration/password/PasswordResetToken.java
@@ -1,4 +1,32 @@
 package com.end2end.application.registration.password;
 
+import com.end2end.application.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class PasswordResetToken {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private Long id;
+
+    private String token;
+    private Date expirationTime;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public PasswordResetToken(String token, User user) {
+        this.token = token;
+        this.expirationTime = null;
+        this.user = user;
+    }
 }
